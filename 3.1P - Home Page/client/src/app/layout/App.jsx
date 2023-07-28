43a253ac9d24191488793298a1ee73f0d42e0d9f
ejbito <138738056+ejbito@users.noreply.github.com>
2023-07-28 12:52:40 +1000

    Start 3.1P

---
+++ 3.1P - Home Page/client/src/app/layout/App.jsx
@@ -0,0 +1,12 @@
+import { Container, CssBaseline, ThemeProvider } from "@mui/material";
+import Header from "./Header";
+
+function App() {
+  return (
+    <CssBaseline />
+    <Header />
+    <Container></Container>
+  );
+}
+
+export default App;
